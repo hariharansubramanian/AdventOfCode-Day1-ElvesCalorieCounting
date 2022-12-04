b4b9aef7b0b308fff3c1991d410abe293de8e180
Hariharan Subramanian <hari@flowinsights.com>
2022-12-04 14:04:26 +0100

    Calculating top 3 Elf's calorie counts and their total

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["ElvesCalorieCounter.csproj", "./"]
+RUN dotnet restore "ElvesCalorieCounter.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "ElvesCalorieCounter.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ElvesCalorieCounter.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ElvesCalorieCounter.dll"]
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Counting calories...");
+
+var calorieEntries = File.ReadLines("calorie_list.txt").ToList();
+
+var elvesCalorieCountList = new List<long>();
+
+long elfCalorieCount = 0;
+foreach (var calorieEntry in calorieEntries)
+{
+    if (calorieEntry != "") elfCalorieCount += long.Parse(calorieEntry);
+    else
+    {
+        elvesCalorieCountList.Add(elfCalorieCount);
+        elfCalorieCount = 0;
+    }
+}
+
+var elvesCalorieCountListSorted = elvesCalorieCountList.OrderByDescending(x => x).ToList();
+var top3Calories = elvesCalorieCountListSorted.Take(3).ToList();
+var top3CalorieCount = top3Calories.Sum(x => x);
+
+Console.WriteLine($"The top 3 calories counted are {string.Join(',', top3Calories.Select(x => x))}");
+Console.WriteLine($"The total calories counted are {top3CalorieCount}");
